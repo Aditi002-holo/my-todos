fc271687558e61c0222e069fc9c367be62eadfcc
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-29 16:31:35 +0530

    Add checkbox & delete btn functionality

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const container = document.querySelector(".container")
 const taskInput = document.getElementById("task-input");
 const addBtn = document.getElementById("add-button");
 
-
 addBtn.addEventListener("click",function() {
     let taskValue = taskInput.value;
-    const taskContainer = document.createElement('div');
-    taskContainer.classList.add('task-container');
-    taskContainer.innerHTML = `
-        <input id="checkbox" type="checkbox">
-        <p id="task">${taskValue}</p>
-        <button id="del-btn" class="del-btn">
-            <i class="fa-solid fa-trash"></i>
-        </button>`
 
-    container.appendChild(taskContainer);
-})
+    if(taskValue === "") {
+        alert("Enter a task");
+    } else {
+        const taskContainer = document.createElement('div');
+        taskContainer.classList.add('task-container');
+        taskContainer.innerHTML = `
+            <input id="checkbox" class="checkbox" type="checkbox">
+            <p id="task" class="task">${taskValue}</p>
+            <button id="del-btn" class="del-btn">
+                <i class="fa-solid fa-trash"></i>
+            </button>`
+
+        container.appendChild(taskContainer);
+
+        taskContainer.addEventListener("click", completeOrDeleteTasks)
+    }
+});
+
+function completeOrDeleteTasks(e) {
+    const targetEl = e.target;
+    if(targetEl.classList.contains('checkbox')) {
+        if(targetEl.checked) {
+            targetEl.nextElementSibling.classList.add("task-clicked")
+        } else {
+            targetEl.nextElementSibling.classList.remove("task-clicked");
+        }
+    }
+
+    if(targetEl.parentElement.classList.contains('del-btn')) {
+        e.currentTarget.remove();
+    }
+}
+
